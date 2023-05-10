677e9d598c5940ae4257a3bcd717c439b39dd7b8
Charlie <charlie@Charlies-MBP.broadband>
2023-05-10 20:22:34 +0100

    bug fix and add tests

--- tests/test_takehomepay.py
+++ tests/test_takehomepay.py
@@ -4,4 +4,6 @@ from takehomepay import takehomepay
 
 def test_twentytwentytwo():
     tt = takehomepay.TwentyTwentyTwo()
-    print(tt.calculate_tax(25_000, 0))
+    assert float(tt.calculate_tax(20_000, 0)) == 1486.0, "Error on 20k"
+    assert float(tt.calculate_tax(60_000, 0)) == 11_432.0, "Error on 60k"
+    assert float(tt.calculate_tax(200_000, 0)) == 74_960.0, "Error on 200k"
