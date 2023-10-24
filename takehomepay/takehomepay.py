f6e24d3b94c898715c186bd01fbbfc11405d91b8
Charlie <charlie@Charlies-MBP.broadband>
2023-10-24 15:34:48 +0100

    add docstrings

--- takehomepay/takehomepay.py
+++ takehomepay/takehomepay.py
@@ -14,61 +14,122 @@ class TakeHomePayBase:
     lower_earnings_limit_ni: int  # 0% up to this limit
     upper_earnings_limit_ni: int  # 12% up to this limit, then 2% after
 
-    def _calculate_basic_tax(self, gross, deductions):
+    def _calculate_basic_tax(self, gross: float, deductions: float) -> float:
+        """calculate basic rate tax.
+
+        Args:
+            gross (float): gross salary
+            deductions (float): any non taxable deductions
+
+        Returns:
+            float: value of basic rate tax
+        """
         gross_minus_deductions = min(
             gross - deductions - self.personal_allowance,
             self.basic_rate - self.personal_allowance,
         )
         if gross_minus_deductions < 0:
-            return 0
+            return 0.0
         else:
             tax = (gross_minus_deductions / 100) * self.basic_rate_percentage
             return tax
 
-    def _calculate_higher_tax(self, gross, deductions):
+    def _calculate_higher_tax(self, gross: float, deductions: float) -> float:
+        """calculate higher rate tax.
+
+        Args:
+            gross (float): gross salary
+            deductions (float): any non taxable deductions
+
+        Returns:
+            float: value of higher rate tax
+        """
         if gross > 100_000:  # lose personal allowance if greater than 100k
             self._calculate_tax_code(gross)
-        else:
-            gross_minus_deductions = min(
-                gross - deductions - self.basic_rate, self.higher_rate - self.basic_rate
-            )
+            ## HANDLE THIS
+
+        gross_minus_deductions = min(
+            gross - deductions - self.basic_rate, self.higher_rate - self.basic_rate
+        )
 
         if gross_minus_deductions < 0:
-            return 0
+            return 0.0
         else:
             tax = (gross_minus_deductions / 100) * self.higher_rate_percentage
             return tax
 
-    def _calculate_additional_tax(self, gross, deductions):
+    def _calculate_additional_tax(self, gross: float, deductions: float) -> float:
+        """calculate additional rate tax.
+
+        Args:
+            gross (float): gross salary
+            deductions (float): any non taxable deductions
+
+        Returns:
+            float: value of higher rate tax
+        """
         gross_minus_deductions = gross - deductions - self.higher_rate
         if gross_minus_deductions < 0:
-            return 0
+            return 0.0
         else:
             tax = (gross_minus_deductions / 100) * self.additional_rate_percentage
             return tax
-        
-    def _calculate_tax_code(self, gross):
+
+    def _calculate_tax_code(self, gross: float) -> None:
+        """when salary is greater than 100k, tax code is reduced. This will work out
+        the new tax code.
+
+        Args:
+            gross (float): gross salary
+        """
         if gross > self.personal_allowance_upper:
             diff = gross - self.personal_allowance_upper
 
         if diff > 0:
             personal_allowance_reduction = diff / 2
-        
+
             if personal_allowance_reduction >= self.personal_allowance:
                 self.personal_allowance = 0
             else:
-                self.personal_allowance = self.personal_allowance - personal_allowance_reduction
+                self.personal_allowance = (
+                    self.personal_allowance - personal_allowance_reduction
+                )
 
-    def calculate_tax(self, gross, deductions):
-        basic_tax = self._calculate_basic_tax(gross, deductions)
-        higher_tax = self._calculate_higher_tax(gross, deductions)
-        additional_tax = self._calculate_additional_tax(gross, deductions)
+    def calculate_tax(
+        self, gross: float, deductions: float, return_breakdown: bool = False
+    ) -> float:
+        """calculate UK tax to pay on a gross salary whilst taking into consideration
+        and non-taxable deductions.
 
-        total_tax = basic_tax + higher_tax + additional_tax
+        Args:
+            gross (float): gross salary
+            deductions (float): any non taxable deductions
+            return_breakdown (bool): False
 
-        return total_tax
+        Returns:
+            float: value of higher rate tax
+        """
+        tax_dict = {
+            "basic_tax": self._calculate_basic_tax(gross, deductions),
+            "higher_tax": self._calculate_higher_tax(gross, deductions),
+            "additional_tax": self._calculate_additional_tax(gross, deductions),
+        }
 
-    def calculate_national_insurance(self, gross, freq: str = "yearly"):
+        if return_breakdown is True:
+            return tax_dict
+        else:
+            return sum(tax_dict.values())
+
+    def calculate_national_insurance(self, gross: float, freq: str = "yearly") -> float:
+        """calculate UK national insurance to pay on gross salary.
+
+        Args:
+            gross (float): gross salary
+            freq (str, optional): return total as a "weekly" or "yearly" value. Defaults to "yearly".
+
+        Returns:
+            float: _description_
+        """
         weekly = gross / 52
 
         if int(weekly) in range(0, 190):
@@ -82,7 +143,7 @@ class TakeHomePayBase:
 
         if freq == "yearly":
             return ni * 52
-        
+
         elif freq == "weekly":
             return ni
 
@@ -103,7 +164,7 @@ class TwentyTwentyTwo(TakeHomePayBase):
     lower_earnings_limit_ni: int = 6  # 0% up to this limit
     upper_earnings_limit_ni: int = 7  # 12% up to this limit, then 2% after
 
-    def __post_init__(self):
+    def __post_init__(self) -> None:
         super().__init__(
             self.personal_allowance,
             self.personal_allowance_upper,
